5b82809704d52cacd6a17fc0348baf5f2b16e456
phellweg <peter.hellwege@vizientinc.com>
2022-05-20 10:26:35 -0600

    header done

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import logo from '../static/logo.svg';
+import cart from '../static/icon-cart.svg';
+import avatar from '../static/image-avatar.png'
+import './NavBar.css';
+
+export default (props) => {
+        
+    return (
+        <div id="navBarWrap" className="sticky">
+            <div id='navBarLinks'>
+            <ul >
+                <li><a href='/'><img src={logo} alt='Logo'/></a></li>
+                <li><a href='/' id='navText'> Collections</a></li>
+                <li><a href='/' id='navText'> Men</a></li>
+                <li><a href='/' id='navText'> Women</a></li>
+                <li><a href='/' id='navText'> About</a></li>
+                <li><a href='/' id='navText'> Contact</a></li>
+            </ul>
+            </div>
+            <div id='header-right'>
+                <div>
+                    <a href='/'><img src={cart} alt='Logo'/></a>
+                </div>
+                <div>
+                    <a href='/'><img src={avatar} id='avatar' alt='Logo'/></a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
---
+++ src/views/ProductPage.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import Header from '../components/Header'
+export default (props) => {
+        
+    return (
+        <>
+            <Header/>
+        </>
+    )
+}
+
