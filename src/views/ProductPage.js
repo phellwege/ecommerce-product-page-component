468a486f7bf62d8a47b1cf3cc70f1b6a43a5c7a7
phellweg <peter.hellwege@vizientinc.com>
2022-05-20 10:41:43 -0600

    start main page

--- src/views/ProductPage.js
+++ src/views/ProductPage.js
@@ -1,10 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../components/Header'
+import './Product.css'
+import product from '../static/image-product-1.jpg'
 export default (props) => {
         
     return (
         <>
             <Header/>
+            <div className='ProductWrap'>
+                <div className="LeftDiv">
+                    <div id='BigImg'>
+                        <img src={product}/>
+                    </div>
+                    <div id='ImgSelector'>
+                        selected img 
+                    </div>
+                </div>
+                <div className="RightDiv">
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
         </>
     )
 }
